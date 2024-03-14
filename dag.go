acde59043610392f06a97e88ec5ee21b655d8a48
ming200202 <627824203@qq.com>
2024-03-14 14:01:20 +0800

    Initial commit

--- dag.go
+++ dag.go
@@ -14,6 +14,22 @@ type Object struct {
 }
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+	switch node.Type() {
+	case FILE:
+		fileNode := node.(File)
+		store.Put([]byte("content"), fileNode.Bytes())
+		h.Write(fileNode.Bytes())
+		return h.Sum(nil)
+	case DIR:
+		dirNode := node.(Dir)
+		iterator := dirNode.It()
+		for iterator.Next() {
+			childNode := iterator.Node()
+			childMerkleRoot := Add(store, childNode, h)
+			h.Write(childMerkleRoot)
+		}
+		return h.Sum(nil)
+	default:
+		return nil
+	}
 }
