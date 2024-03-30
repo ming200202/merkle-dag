42503925bfe25422b923532862cb358affac3963
ming200202 <627824203@qq.com>
2024-03-31 00:15:55 +0800

    second commit

--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,52 @@
 package merkledag
 
+import (
+	"encoding/json"
+	"strings"
+)
+
 // Hash to file
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
+
+	exists, err := store.Has(hash)
+	if err != nil {
+		// Handle error
+		return nil
+	}
+	if !exists {
+		// Hash does not exist in the KVStore
+		return nil
+	}
+
+	data, err := store.Get(hash)
+	if err != nil {
+		// Handle error
+		return nil
+	}
+	var tree map[string]interface{}
+	if err := json.Unmarshal(data, &tree); err != nil {
+		// Handle error
+		return nil
+	}
+
+	var currentNode interface{} = tree
+	components := strings.Split(path, "/")
+	for _, component := range components {
+		if dir, ok := currentNode.(map[string]interface{}); ok {
+
+			node, found := dir[component]
+			if !found {
+				return nil // Component not found in directory
+			}
+			currentNode = node
+		} else {
+			return nil // Current node is not a directory
+		}
+	}
+
+	if file, ok := currentNode.([]byte); ok {
+		return file
+	}
+
 	return nil
 }
